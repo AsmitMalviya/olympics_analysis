e6987f447815143bfce0b15bdd2f92f98ca5101b
AsmitMalviya <168575678+AsmitMalviya@users.noreply.github.com>
2025-01-14 17:25:05 +0000

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import openai
 
 def fetch_medal_tally(df, year, country):
     medal_df = df.drop_duplicates(subset=['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Event', 'Medal'])
